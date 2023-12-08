ece23e6ccde7074682b5c4484974a586e89e98e1
AminovaAnna <aminovaanna163@gmail.com>
2023-12-08 10:22:24 +0300

    initial commit

---
+++ src/griffindor.java
@@ -0,0 +1,35 @@
+public class griffindor {
+    int nobility;
+    int honor;
+    int brave;
+
+    public griffindor(int nobility, int honor, int brave) {
+        this.nobility = nobility;
+        this.honor = honor;
+        this.brave = brave;
+    }
+
+    public int getNobility() {
+        return nobility;
+    }
+
+    public void setNobility(int nobility) {
+        this.nobility = nobility;
+    }
+
+    public int getHonor() {
+        return honor;
+    }
+
+    public void setHonor(int honor) {
+        this.honor = honor;
+    }
+
+    public int getBrave() {
+        return brave;
+    }
+
+    public void setBrave(int brave) {
+        this.brave = brave;
+    }
+}
